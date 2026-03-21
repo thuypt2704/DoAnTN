2bcb4e1d0815998377ec73a30af5bcae38a614ca
Thuy Phung <thuythattha2704@gmail.com>
2026-03-21 18:03:46 +0700

    Update: Update code

---
+++ Assets/Scripts/Enums/Enums.cs
@@ -0,0 +1,13 @@
+public enum ToolEffect
+{
+    none,
+    watering
+}
+public enum Direction
+{
+    up,
+    down,
+    left,
+    right,
+    none
+}
---
+++ Assets/Scripts/Events/EventHandler.cs
@@ -0,0 +1,39 @@
+
+using UnityEngine.Rendering;
+
+public delegate void MovementDelegate(float inputX, float inputY,
+    bool isWalking, bool isRunning, bool isIdle, bool isCarrying,
+    ToolEffect toolEffect,
+    bool isUsingToolRight, bool isUsingToolLeft, bool isUsingToolUp, bool isUsingToolDown,
+    bool isLiftingToolRight, bool isLiftingToolLeft, bool isLiftingToolUp, bool isLiftingToolDown,
+    bool isPickingRight, bool isPickingLeft, bool isPickingUp, bool isPickingDown,
+    bool isSwingingToolRight, bool isSwingingToolLeft, bool isSwingingToolUp, bool isSwingingToolDown,
+    bool idleRight, bool idleLeft, bool idleUp, bool idleDown);
+public static class EventHandler
+{
+    //Movement Event
+
+    public static event MovementDelegate MovementEvent;
+
+    //Movement Event call For Publishers
+
+    public static void CallMovementEvent(float inputX, float inputY,
+    bool isWalking, bool isRunning, bool isIdle, bool isCarrying,
+    ToolEffect toolEffect,
+    bool isUsingToolRight, bool isUsingToolLeft, bool isUsingToolUp, bool isUsingToolDown,
+    bool isLiftingToolRight, bool isLiftingToolLeft, bool isLiftingToolUp, bool isLiftingToolDown,
+    bool isPickingRight, bool isPickingLeft, bool isPickingUp, bool isPickingDown,
+    bool isSwingingToolRight, bool isSwingingToolLeft, bool isSwingingToolUp, bool isSwingingToolDown,
+    bool idleRight, bool idleLeft, bool idleUp, bool idleDown)
+    {
+        if(MovementEvent != null)
+            MovementEvent(inputX, inputY, 
+                isWalking, isRunning, isIdle, isCarrying, 
+                toolEffect,
+                isUsingToolRight, isUsingToolLeft, isUsingToolUp, isUsingToolDown,
+                isLiftingToolRight, isLiftingToolLeft, isLiftingToolUp, isLiftingToolDown,
+                isPickingRight, isPickingLeft, isPickingUp, isPickingDown,
+                isSwingingToolRight, isSwingingToolLeft, isSwingingToolUp, isSwingingToolDown,
+                idleRight, idleLeft, idleUp, idleDown );
+    }
+}
