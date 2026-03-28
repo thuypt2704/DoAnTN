6d61ba114381fc322a5ca7f1e76696b56fb3bf37
Thuy Phung <thuythattha2704@gmail.com>
2026-03-28 20:04:15 +0700

    Update day: Add Item Details Class

--- Assets/Scripts/Enums/Enums.cs
+++ Assets/Scripts/Enums/Enums.cs
@@ -11,3 +11,18 @@ public enum Direction
     right,
     none
 }
+public enum ItemType
+{
+    Seed,
+    Commodity,
+    Watering_tool,
+    Hoeing_tool,
+    Chopping_tool,
+    Breaking_tool,
+    Reaping_tool,
+    Collecting_tool,
+    Reapable_scenary,
+    Furniture,
+    none,
+    count
+}
